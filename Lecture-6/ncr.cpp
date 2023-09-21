e632d2b36671e98a916b0ff2c3d5f1b921a1960e
Harshada Samane <samaneharshada04@gmail.com>
2023-09-21 23:41:40 +0530

    Added ncr.cpp to lect-6.
    Learned how to find ncr using function.

--- Lecture-6/ncr.cpp
+++ Lecture-6/ncr.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 using namespace std;
+
+int fact(int n) {
+    int factorial = 1;
+    for(int i=2; i<=n; i++) {
+        factorial = factorial * i;
+    }
+    return factorial;
+
+}
+int main() {
+    int n,r;
+    cout<<"Enter n: \n";
+    cin>>n;
+    cout<<"Enter r: ";
+    cin>>r;
+
+    int ans = fact(n)/(fact(r)*fact(n-r));
+    cout<<n<<"c"<<r<<"="<<ans;
+}
