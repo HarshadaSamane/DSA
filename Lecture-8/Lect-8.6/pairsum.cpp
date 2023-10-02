9e233812fc4ac8fa826c998b83ec8a5c0ccf0472
Harshada Samane <samaneharshada04@gmail.com>
2023-10-03 00:01:02 +0530

    updated pairsum.
    Reduced the complexity of code.

--- Lecture-8/Lect-8.6/pairsum.cpp
+++ Lecture-8/Lect-8.6/pairsum.cpp
@@ -3,17 +3,40 @@
 #include <climits>
 using namespace std;
 
-bool pairsum(int arr[], int n, int k) {
+// bool pairsum(int arr[], int n, int k) {
 
-    for(int i=0; i<n-1; i++) {
-        for(int j=i+1; j<n; j++) {
-            if(arr[i]+arr[j]==k) {
-                cout<<"position of first element is: "<<i<<endl<<"position of second element is: "<<j<<endl;
-                return true;
-            }
+//     for(int i=0; i<n-1; i++) {
+//         for(int j=i+1; j<n; j++) {
+//             if(arr[i]+arr[j]==k) {
+//                 cout<<"position of first element is: "<<i<<endl<<"position of second element is: "<<j<<endl;
+//                 return true;
+//             }
+//         }
+//     }
+
+//     return false;
+// }
+
+bool pairsum (int arr[], int n, int k) {
+    // Reduced the complexity of code. 
+    // The elements in array should be in ascending order.
+
+    int low = 0;
+    int high = n-1;
+
+    while(low<high) {
+        if(arr[low]+arr[high]==k) {
+            cout<<"position of first element: "<<low<<endl<<"position of second element: "<<high<<endl;
+            cout<<"sum of numbers is: "<<k<<endl;
+            return true;
+        }
+        else if(arr[low]+arr[high]>k) {
+            high--;
+        }
+        else {
+            low++;
         }
     }
-
     return false;
 }
 
