713b51490d300458944496d7a2c49cce9e73283c
Harshada Samane <samaneharshada04@gmail.com>
2023-10-15 22:57:29 +0530

    Added to lecture-14
    Learn to find a subset of an array and the number of ones

--- Lecture-14/bit2.cpp
+++ Lecture-14/bit2.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace std;
 //write a program to check if a given no. is power of 2 
-int main() {
+bool ispowerof2(int n) {
+    return (n && !(n & n-1)); //true
+}
 
+int main() {
+        cout << ispowerof2(16) << endl;
+
+    return 0;
 }
